2ff997d6f39877545c430e02c24570911b0403d8
David DeWitt <dkdewitt@gmail.com>
2016-06-20 09:54:57 -0700

    Fixed formmating issue

--- app/containers/App/reducers.js
+++ app/containers/App/reducers.js
@@ -1,12 +1,12 @@
-//import reducers from '<project-path>/reducers'
-import {sample} from '../Sample/reducers';
-// Add the reducer to your store on the `routing` key
+
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
-  const rootReducer = combineReducers({
-    sample,
-    routing: routerReducer
-  })
+import {sample} from '../Sample/reducers';
+
+const rootReducer = combineReducers({
+	sample,
+	routing: routerReducer
+})
 
 
 export default rootReducer;
